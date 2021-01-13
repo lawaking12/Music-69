7361641ef3f23fde561f376ef74306e6526f284f
LeVeL <72306257+LeVeL159@users.noreply.github.com>
2021-01-13 15:36:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('NzU1NzY4NTkyMTA0MDMwMjA4.X2IGWQ.F9NOzwrvVPldokUN-jXpwVwm7Ko');
+client.login('');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
