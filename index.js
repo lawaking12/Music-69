5f34c00597a96556457aed6ca28296a8a4f58b61
darom99 <70956604+darom99@users.noreply.github.com>
2021-02-26 07:55:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ client.on(`message`, async (message) => {
     .setColor("RANDOM")
     .setThumbnail(message.author.avatarURL({dynamic: "true"}))
       .setAuthor("Help Commands!","https://cdn.discordapp.com/emojis/775437938142085170.gif?v=1&size=64")
-    .setImage('https://cdn.discordapp.com/attachments/793639750170443787/798756675342303262/image0.gif')
+    .setImage('https://cdn.discordapp.com/attachments/793639750170443787/814722052569170010/image0.gif')
     
     .setTitle("NEW UPDATE IN PLAY🎵")
     .setFooter(`Requested by: ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
