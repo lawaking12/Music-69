92468735f5054e4fd3bc4a8a676903b77cdcc8ef
lawaking12 <78676508+lawaking12@users.noreply.github.com>
2021-04-04 15:12:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ client.on(`ready`, () => {
       });
       
 
-    client.user.setActivity(`Type: ${PREFIX}help | ${client.guilds.cache.size} Server,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "PLAYING"});
+    client.user.setActivity(`${PREFIX}help`, { type: "PLAYING"});
    
   
       }, (5000));
